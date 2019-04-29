e4f345d32fb0c5ab2dd76291b40c81a556d04831
ssavin <ssavin@solit-clouds.ru>
2019-04-29 16:06:02 +0300

    80 port back

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('/districts', function(req, res){
     })
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
   });
