50f2ee1e917b97815e3b2a2094a2e00374afa539
Sergey Savin <alter4444@gmail.com>
2019-07-03 13:24:38 +0300

    body parser, delete, post, edit mode for entity

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 let express = require('express');
 const cors = require('cors');
+const bodyparser = require('body-parser');
+const jsonparser = bodyparser.json();
+
 let app = express();
 let mysql = require('mysql');
 let host = process.env.HOST || 'localhost';
@@ -33,6 +36,16 @@ const entities = {
                 type: 'id',
                 db_name: 'dict_category_service'
             },
+        ],
+        fields:[ 
+            { key: 'id', type: 'id' }, 
+            { key: 'title', type: 'string', required: true }, 
+            { key: 'category', type: 'id', required: true }, 
+            { key: 'description', type: 'string' }, 
+            { key: 'article_id', type: 'id' }, 
+            { key: 'gallery_id', type: 'id' }, 
+            { key: 'trimester', type: 'id' }, 
+            { key: 'type', type: 'id' }, 
         ]
     },
     ent_clinics: {
@@ -84,6 +97,103 @@ entity.get('/', cors(), function(req, res){
 
 });
 
+function concatFn(arrA, arrB){
+    console.log('A:', arrA, 'B:', arrB);
+    if( arrA && arrA.length && arrB && arrB.length ){
+        let fine = [];
+        for(let i = 0; i < arrA.length; i++){
+            fine.push(`${arrA[i]} = ${arrB[i]}`);
+        }
+        return fine;
+    }
+    return [];
+}
+
+function createEntity(req, res, next){
+    console.log('post middle', req.body);
+    if( req.body ){
+        //проверка наличия сущности в системе
+        if( !!entities[req.params.id] && !!entities[req.params.id].db_name ){
+            const db = entities[req.params.id].db_name;
+            const reqKeys = entities[req.params.id].fields.filter(f => !!f.required);
+            const fields = entities[req.params.id].fields;
+            const data = req.body;
+
+            if( !reqKeys.every(r => !!data[r.key]) ) {
+                res.end('не полные данные в запросе');
+                return;
+            }
+
+            if( !Object.keys(data).every(r => !!fields.find(f => f.key === r ))) {
+                res.end('в запросе присутствут неизвестные поля');
+                return;
+            }
+
+            let valArr = Object.keys(data).map(datakey => {
+                const targetReq = fields.find(r => r.key === datakey);
+                if(!targetReq) return `"${data[datakey]}"`;
+                return targetReq.type === 'string' ? `"${data[datakey]}"` : data[datakey];
+            });
+
+            let existArr = concatFn( Object.keys(data), valArr );
+            //console.log('existArr: ', existArr);
+
+            const q = `INSERT INTO \`${ db }\` (\`${ Object.keys(data).join('\`, \`') }\`) VALUES ( ${ valArr.join(',') } )`;
+            const qi = existArr.join(', ');
+            //console.log('q: ', q, 'qi:', qi);
+
+
+            pool.query(q + ' ON DUPLICATE KEY UPDATE ' + qi, (err, result)=> {
+                if(err) {
+                    res.status(500);
+                    res.send(err);
+                    return;
+                }
+                res.send(JSON.stringify({
+                    result,
+                    data
+                }));
+                next();
+            });
+
+        } else {
+            res.end('не удалось определить сущность');
+        }
+    }
+}
+
+function deleteEntity(req, res, next){
+    console.log('delete middle', req.body);
+    if( req.body ){
+        //проверка наличия сущности в системе
+        if( !!entities[req.params.id] && !!entities[req.params.id].db_name ){
+            const db = entities[req.params.id].db_name;
+            const id = req.body && req.body.id;
+
+            const qd = `DELETE FROM \`${ db }\` WHERE id=${id}`;
+
+            pool.query(qd, (err, result)=> {
+                if(err) {
+                    res.status(500);
+                    res.send(err);
+                    return;
+                }
+                res.send(JSON.stringify({
+                    result,
+                    text:`Запись с id = ${id} удалена`
+                }));
+
+                next();
+
+            });
+
+        } else {
+            res.end('не удалось определить сущность');
+        }
+    }
+}
+
+
 entity.get('/:id/filters', cors(), function(req, res){
     if( !!entities[req.params.id] && !!entities[req.params.id].filters ){
         res.send( JSON.stringify( entities[req.params.id].filters ) )
@@ -123,6 +233,14 @@ entity.get('/:id', cors(), function(req, res){
     } else {
         res.send([]);
     }
+});
+
+entity.delete('/:id', cors(), jsonparser, deleteEntity, function(req, res){
+    res.end('delete done');
+});
+
+entity.post('/:id', cors(), jsonparser, createEntity, function(req, res){
+    res.end('post done');
 });
 
 admin.use('/dict', dict);
