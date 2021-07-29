14acfcc1b0c097935ac670cb769bd33bcfa6caeb
Sergey Savin <alter4444@gmail.com>
2021-07-29 17:51:46 +0400

    wip search engine

--- search.engine/engine.ts
+++ search.engine/engine.ts
@@ -7,6 +7,32 @@ import {zip} from "rxjs";
 import {map} from "rxjs/operators";
 import {md5Encript} from "./sections.handler";
 
+interface Hashed <T>{
+    [hash: string]: T[]
+}
+type SectionKeys = keyof typeof sectionClinicConfig;
+type TypeSection<T> = {
+    [k in SectionKeys]: Hashed<T>
+}
+
+export interface SearchStore extends TypeSection<any> {}
+export interface SetStore extends TypeSection<Setted> {}
+
+export interface SlotPriceSet {
+    min_price: number;
+    max_price: number;
+    avg_price: number;
+    total_slots: number;
+}
+
+export interface Rating {
+    min_rate: number;
+    max_rate: number;
+    avg_rate: number;
+    count_votes: number;
+}
+
+export type Setted = (SlotPriceSet | Rating) & {id: number};
 
 export class SearchEngine {
 
@@ -14,6 +40,13 @@ export class SearchEngine {
     cache: CacheEngine;
     configSection = sectionClinicConfig;
     searchConfig: SearchConfig;
+
+    searchStore: SearchStore = {
+        clinic: {}
+    };
+    setStore: SetStore = {
+        clinic: {}
+    };
 
     constructor(private _ce: CacheEngine, private _de: DictionaryEngine) {
         const context: Context = {cacheEngine: this._ce, searchEngine: this, dictionaryEngine: this._de};
@@ -26,6 +59,28 @@ export class SearchEngine {
 
     rootHandler(req, res) {
         res.send({index: 'search root index', testMD5: md5Encript({test: 'hello-world'})});
+    }
+
+    setDataStore(section: SectionKeys, hash: string, data: any): void {
+        if (!this.checkSectionExist(section)) {
+            this.searchStore[section] = {};
+        }
+        this.searchStore[section][hash] = data;
+    }
+
+    checkSectionExist(section: SectionKeys): boolean {
+        return !!this.searchStore[section];
+    }
+
+    setSummaryStore(section: SectionKeys, hash: string, data: any): void {
+        if (!this.checkSummarySectionExist(section)) {
+            this.setStore[section] = {};
+        }
+        this.setStore[section][hash] = data;
+    }
+
+    checkSummarySectionExist(section: SectionKeys): boolean {
+        return !!this.setStore[section];
     }
 
     sendFiltersHandler(req, res): void {
